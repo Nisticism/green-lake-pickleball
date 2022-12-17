6f9c0d6310597e8c96f87294d8d70a9e2c6493ef
hfoster00 <94389711+hfoster00@users.noreply.github.com>
2022-12-17 01:23:00 -0600

    testing photos

--- src/helpers/albumData.js
+++ src/helpers/albumData.js
@@ -4,13 +4,13 @@ let albumData = {
     title: "Miscellaneous",
     alts: 
       ["Paddle stacks at Green Lake", 
-      "Green Lake East pickleball courts on a cloudy day", "test alt", "test alt 2"],
+      "Green Lake East pickleball courts on a cloudy day"],
     captions: 
       ["Sometimes when it gets very crowded, there are many paddle stacks on the side of the court.",
-      "Green Lake courts on a cloudy weekend afternoon, a few days after pressure washing.", "test caption", "test caption 2"], 
+      "Green Lake courts on a cloudy weekend afternoon, a few days after pressure washing."], 
     photos: 
       ['/images/misc/paddle_stacks.png', 
-      '/images/misc/cloudy_green_lake.jpg', '/images/misc/nyt_paddle_stacks_1.jpg', '/images/misc/nyt_copy.jpg']},
+      '/images/misc/cloudy_green_lake.jpg']},
   albumDataNYT: {
     title: "New York Times Photos",
     alts: 
@@ -28,8 +28,7 @@ let albumData = {
       "Photo from GL East courts, Seattle - NY Times and Alan Brener", "Photo from GL East courts, Seattle - NY Times and Alan Brener", 
       "Photo from GL East courts, Seattle - NY Times and Alan Brener", "Photo from GL East courts, Seattle - NY Times and Alan Brener", 
       "Photo from GL East courts, Seattle - NY Times and Alan Brener", "Photo from GL East courts, Seattle - NY Times and Alan Brener", 
-      "Photo from GL East courts, Seattle - NY Times and Alan Brener", "Photo from GL East courts, Seattle - NY Times and Alan Brener", "hanger",
-    "nyt copy"],
+      "Photo from GL East courts, Seattle - NY Times and Alan Brener", "Photo from GL East courts, Seattle - NY Times and Alan Brener"],
     captions: 
       ["", "", "", "",
       "", "", "", "",
@@ -38,16 +37,16 @@ let albumData = {
       "", "", "", "",
       "", "", "", "",
       "", "", "", "",
-      "", "", "hanger", "nyt copy"], 
+      "", ""], 
     photos: 
-      ['/images/misc/cloudy_green_lake.jpg', '/images/misc/nyt_paddle_stacks_1.jpg', '/images/nyt/IMG_0030.jpg', '/images/nyt/IMG_0031.jpg',
+      ['/images/nyt/IMG_0028.jpg', '/images/nyt/IMG_0029.jpg', '/images/nyt/IMG_0030.jpg', '/images/nyt/IMG_0031.jpg',
       '/images/nyt/IMG_0032.jpg', '/images/nyt/IMG_0033.jpg', '/images/nyt/IMG_0034.jpg', '/images/nyt/IMG_0035.jpg',
       '/images/nyt/IMG_0036.jpg', '/images/nyt/IMG_0037.jpg', '/images/nyt/IMG_0038.jpg', '/images/nyt/IMG_0039.jpg',
       '/images/nyt/IMG_0040.jpg', '/images/nyt/IMG_0041.jpg', '/images/nyt/IMG_0042.jpg', '/images/nyt/IMG_0043.jpg',
       '/images/nyt/IMG_0044.jpg', '/images/nyt/IMG_0045.jpg', '/images/nyt/IMG_0046.jpg', '/images/nyt/IMG_0047.jpg',
       '/images/nyt/IMG_0048.jpg', '/images/nyt/IMG_0049.jpg', '/images/nyt/IMG_0050.jpg', '/images/nyt/IMG_0051.jpg',
       '/images/nyt/IMG_0052.jpg', '/images/nyt/IMG_0053.jpg', '/images/nyt/IMG_0054.jpg', '/images/nyt/IMG_0055.jpg',
-      '/images/nyt/IMG_0056.jpg', '/images/nyt/IMG_0057.jpg', '/images/nyt/hanger.jpg', 'images/nyt/nyt_copy.jpg'],
+      '/images/nyt/IMG_0056.jpg', '/images/nyt/IMG_0057.jpg'],
       albumCaption: "Credit goes to New York Times and Alan Brener for these photos."
   }
 }
