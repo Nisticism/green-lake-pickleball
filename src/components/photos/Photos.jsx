d52c1acd272175798f9e8e5f0b8c884a09cbb5cc
hfoster00 <94389711+hfoster00@users.noreply.github.com>
2022-12-18 18:16:42 -0600

    album cover

--- src/components/photos/Photos.jsx
+++ src/components/photos/Photos.jsx
@@ -7,7 +7,7 @@ function Photos() {
     misc: 
     {title: "Miscellaneous", url: "/photos/misc", src: "images/misc/paddle_stacks.png"},
     nyt: 
-    {title: "New York Times Photos (2022)", url: "/photos/nyt", src: "images/nyt/img_0028.JPG"}
+    {title: "New York Times Photos (2022)", url: "/photos/nyt", src: "images/nyt/IMG_0028.JPG"}
   }
   return (
     <div className="text-align photos-wrapper">
